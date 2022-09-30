29e8a949fcb9fc13b0386c9ec34c2a3d8aa1d9e9
Scott Isitt <isitts@hotmail.com>
2022-09-29 23:05:58 -0700

    Make some fields nullable in Pages model.

--- MiniAdventureDotNetWeb/Models/Page.cs
+++ MiniAdventureDotNetWeb/Models/Page.cs
@@ -9,16 +9,16 @@ namespace MiniAdventureDotNetWeb.Models
         public string ActionCode { get; set; } = "";
         public string Title { get; set; } = "";
         public string Segment1 { get; set; } = "";
-        public string Segment2 { get; set; } = "";
-        public string Segment3 { get; set; } = "";
-        public string Segment4 { get; set; } = "";
-        public string Segment5 { get; set; } = "";
+        public string? Segment2 { get; set; }
+        public string? Segment3 { get; set; }
+        public string? Segment4 { get; set; }
+        public string? Segment5 { get; set; }
         public string OptionA { get; set; } = "";
-        public string OptionB { get; set; } = "";
-        public string OptionC { get; set; } = "";
+        public string? OptionB { get; set; }
+        public string? OptionC { get; set; }
         public string ActionA { get; set; } = "";
-        public string ActionB { get; set; } = "";
-        public string ActionC { get; set; } = "";
+        public string? ActionB { get; set; }
+        public string? ActionC { get; set; }
     }
 }
 
