abf397e39f33a38dd39916e4070cd9f1dddccb12
Scott Isitt <isitts@hotmail.com>
2022-10-02 21:21:46 -0700

    Add a Page Index link to the navbar.

--- MiniAdventureDotNetWeb/Controllers/HomeController.cs
+++ MiniAdventureDotNetWeb/Controllers/HomeController.cs
@@ -16,14 +16,6 @@ public class HomeController : Controller
     //}
 
 
-    //private readonly ILogger<HomeController> _logger;
-
-    //public HomeController(ILogger<HomeController> logger)
-    //{
-    //    _logger = logger;
-    //}
-
-
 
     public IActionResult Index()
     {
